0ff31995bd9844ac7d5d129ad287edf050bd4dc1
starlying <starlying@gmail.com>
2019-05-19 05:24:07 +0800

    netcore3.0

--- SiteServer.API/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SiteServer.API.Models
-{
-    public class TodoItem
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
---
+++ src/SiteServer.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiteServer.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
