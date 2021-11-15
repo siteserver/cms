21feacd997c04122c964b78cc368a41b590ccb3c
timlai <timlai99@outlook.com>
2021-11-15 17:47:36 +0800

    bugfix

--- src/SSCMS.Web/wwwroot/sitefiles/assets/js/admin/cms/editor.js
+++ src/SSCMS.Web/wwwroot/sitefiles/assets/js/admin/cms/editor.js
@@ -5,6 +5,26 @@ var $urlPreview = $url + '/actions/preview';
 var $urlCensor = $url + '/actions/censor';
 var $urlTags = $url + '/actions/tags';
 
+Date.prototype.Format = function (fmt) {
+  var o = {
+    "M+": this.getMonth() + 1,                   // 月份
+    "d+": this.getDate(),                        // 日
+    "h+": this.getHours(),                       // 时
+    "m+": this.getMinutes(),                     // 分
+    "s+": this.getSeconds(),                     // 秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+    "S": this.getMilliseconds()                  // 毫秒
+  };
+
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+
+  return fmt;
+};
+
 var data = utils.init({
   siteId: utils.getQueryInt('siteId'),
   channelId: utils.getQueryInt('channelId'),
@@ -139,9 +159,9 @@ var methods = {
       $this.form = _.assign({}, res.content);
 
       if (!$this.form.addDate) {
-        $this.form.addDate = new Date();
+        $this.form.addDate = new Date().Format("yyyy-MM-dd hh:mm:ss");
       } else {
-        $this.form.addDate = new Date($this.form.addDate);
+        $this.form.addDate = new Date($this.form.addDate).Format("yyyy-MM-dd hh:mm:ss");
       }
 
       if ($this.form.checked) {
