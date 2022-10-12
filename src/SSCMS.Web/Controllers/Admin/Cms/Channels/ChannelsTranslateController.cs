f70d40f653ab205e7adc7cb1456d4be0dc081c72
timlai <timlai99@outlook.com>
2022-10-12 17:21:57 +0800

    bugfix

--- src/SSCMS.Web/Controllers/Admin/Cms/Channels/ChannelsTranslateController.cs
+++ src/SSCMS.Web/Controllers/Admin/Cms/Channels/ChannelsTranslateController.cs
@@ -66,7 +66,7 @@ namespace SSCMS.Web.Controllers.Admin.Cms.Channels
             public int SiteId { get; set; }
             public IEnumerable<int> ChannelIds { get; set; }
             public int TransSiteId { get; set; }
-            public int TransChannelId { get; set; }
+            public List<int> TransChannelIds { get; set; }
             public ChannelTranslateType TranslateType { get; set; }
             public bool IsDeleteAfterTranslate { get; set; }
         }
