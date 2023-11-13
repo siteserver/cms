6b1c68695bdb0ca74773002ab21714eccc6e865d
timlai <timlai99@outlook.com>
2023-11-13 09:48:53 +0800

    bugfix

--- src/SSCMS/Parse/Libraries.cs
+++ src/SSCMS/Parse/Libraries.cs
@@ -2,7 +2,7 @@
 {
     public static class Libraries
     {
-        public const string AxiosJs = "assets/lib/axios-0.17.1.min.js";
+        public const string AxiosJs = "assets/lib/axios-1.6.1.min.js";
         public const string JqueryJs = "assets/lib/jquery-3.6.4.min.js";
         public const string LayerJs = "assets/lib/layer/layer-3.1.1.js";
         public const string PdfObjectJs = "assets/lib/pdfobject-2.2.4.min.js";
