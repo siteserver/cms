90d166c2009dcf38cbdf60d0327cd5d450d7df52
timlai <timlai99@outlook.com>
2024-02-29 08:41:16 +0800

    bugfix

--- src/SSCMS.Web/Controllers/Admin/Plugins/AddLayerUploadController.Override.cs
+++ src/SSCMS.Web/Controllers/Admin/Plugins/AddLayerUploadController.Override.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SSCMS.Configuration;
 using SSCMS.Core.Utils;
 using SSCMS.Dto;
 using SSCMS.Utils;
@@ -16,19 +17,23 @@ namespace SSCMS.Web.Controllers.Admin.Plugins
                 return Unauthorized();
             }
 
-            // _pluginManager.UnInstall(request.PluginId);
-
             var fileName = PathUtils.RemoveParentPath(request.FileName);
             var filePath = _pathManager.GetTemporaryFilesPath(fileName);
             var pluginPath = _pathManager.GetPluginPath(request.PluginId);
+            var configPath = PathUtils.Combine(pluginPath, Constants.PluginConfigFileName);
+            var configValue = string.Empty;
+            if (FileUtils.IsFileExists(configPath))
+            {
+                configValue = await FileUtils.ReadTextAsync(configPath);
+            }
+
             DirectoryUtils.DeleteDirectoryIfExists(pluginPath);
             DirectoryUtils.CreateDirectoryIfNotExists(pluginPath);
             _pathManager.ExtractZip(filePath, pluginPath);
-
-            
-            // var pluginPath = _pathManager.GetPluginPath(request.PluginId);
-            // DirectoryUtils.CreateDirectoryIfNotExists(pluginPath);
-            // _pathManager.ExtractZip(filePath, pluginPath);
+            if (!string.IsNullOrEmpty(configValue))
+            {
+                await FileUtils.WriteTextAsync(configPath, configValue);
+            }
 
             return new BoolResult
             {
